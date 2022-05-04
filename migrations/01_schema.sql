b31450da0df6c5a8b6caa2067cc2db051ea933a0
symphony <jciurc@gmail.com>
2022-05-04 15:45:55 -0600

    fill schemas with more complete types and default values

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users, properties, reservations, property_reviews;
+DROP TABLE IF EXISTS users, properties, reservations, property_reviews, provinces CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -7,28 +7,36 @@ CREATE TABLE users (
   password VARCHAR(255)
 );
 
+
+CREATE TABLE provinces (
+  code VARCHAR(4) PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
   title VARCHAR(255),
   description TEXT,
-  thumbnail_photo_url VARCHAR(255),
-  cover_photo_url VARCHAR(255),
-  cost_per_night SMALLINT,
-  parking_spaces SMALLINT,
-  number_of_bathrooms SMALLINT,
-  country_id INTEGER,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night SMALLINT NOT NULL DEFAULT 0,
+  parking_spaces SMALLINT NOT NULL DEFAULT 0,
+  number_of_bathrooms SMALLINT NOT NULL DEFAULT 0,
+
+  country VARCHAR(255),
   street VARCHAR(255),
   city VARCHAR(255),
-  province_id INTEGER,
+  province VARCHAR(4) REFERENCES provinces(code),
   postal_code VARCHAR(20),
   active BOOLEAN DEFAULT false
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  start_date DATE,
-  end_date DATE,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
@@ -38,6 +46,6 @@ CREATE TABLE property_reviews (
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
-  rating SMALLINT,
+  rating SMALLINT NOT NULL DEFAULT 0,
   review TEXT
 );
