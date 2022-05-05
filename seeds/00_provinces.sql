21d1f58f57a7636b94212ed2b33fc4517b50e275
symphony <jciurc@gmail.com>
2022-05-04 23:51:07 -0600

    add webapp repo files. install dependencies, pg and dotenv. set up database pool

---
+++ seeds/00_provinces.sql
@@ -0,0 +1,14 @@
+-- provinces lookup
+INSERT INTO provinces (code, name) VALUES ('NL', 'Newfoundland and Labrador');
+INSERT INTO provinces (code, name) VALUES ('PE', 'Prince Edward Island');
+INSERT INTO provinces (code, name) VALUES ('NS', 'Nova Scotia');
+INSERT INTO provinces (code, name) VALUES ('NB', 'New Brunswick');
+INSERT INTO provinces (code, name) VALUES ('QB', 'Quebec');
+INSERT INTO provinces (code, name) VALUES ('ON', 'Ontario');
+INSERT INTO provinces (code, name) VALUES ('MB', 'Manitoba');
+INSERT INTO provinces (code, name) VALUES ('SK', 'Saskatchewan');
+INSERT INTO provinces (code, name) VALUES ('AB', 'Alberta');
+INSERT INTO provinces (code, name) VALUES ('BC', 'British Columbia');
+INSERT INTO provinces (code, name) VALUES ('YT', 'Yukon');
+INSERT INTO provinces (code, name) VALUES ('NT', 'Northwest Territories');
+INSERT INTO provinces (code, name) VALUES ('NU', 'Nunavut');
