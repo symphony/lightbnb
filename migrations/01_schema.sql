9c07bff0389dafeb5c5e28ad8077dbbeafc9f0b9
symphony <jciurc@gmail.com>
2022-05-04 18:37:37 -0600

    add prepopulated 02_seeds.sql

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -2,36 +2,31 @@ DROP TABLE IF EXISTS users, properties, reservations, property_reviews, province
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  email VARCHAR(255),
-  password VARCHAR(255)
-);
-
-
-CREATE TABLE provinces (
-  code VARCHAR(4) PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
-  title VARCHAR(255),
+  title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER NOT NULL DEFAULT 0,
-  parking_spaces SMALLINT NOT NULL DEFAULT 0,
-  number_of_bathrooms SMALLINT NOT NULL DEFAULT 0,
-  number_of_bedrooms SMALLINT NOT NULL DEFAULT 0,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
-  country VARCHAR(255) DEFAULT 'Canada',
-  street VARCHAR(255),
-  city VARCHAR(255),
-  province VARCHAR(4) REFERENCES provinces(code) DEFAULT 'BC',
-  postal_code VARCHAR(20),
-  active BOOLEAN DEFAULT false
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
@@ -44,8 +39,8 @@ CREATE TABLE reservations (
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
